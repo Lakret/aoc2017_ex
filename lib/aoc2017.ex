a2a9c49bd9537719b595b383d451554f6f4f8562
Dmitry Slutsky <rakuretu@gmail.com>
2017-12-07 05:45:03 +0300

    init

---
+++ lib/aoc2017.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2017 do
+  @moduledoc """
+  Documentation for Aoc2017.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2017.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2017_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2017Test do
+  use ExUnit.Case
+  doctest Aoc2017
+
+  test "greets the world" do
+    assert Aoc2017.hello() == :world
+  end
+end
